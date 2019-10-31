0b08b8c2ae17d9fd5cb222680bbf3ec5950cd28f
niku <niku@niku.name>
2019-10-31 21:47:26 +0900

    Create a project
    $ mix new struct_diff

---
+++ lib/struct_diff.ex
@@ -0,0 +1,18 @@
+defmodule StructDiff do
+  @moduledoc """
+  Documentation for StructDiff.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StructDiff.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/struct_diff_test.exs
@@ -0,0 +1,8 @@
+defmodule StructDiffTest do
+  use ExUnit.Case
+  doctest StructDiff
+
+  test "greets the world" do
+    assert StructDiff.hello() == :world
+  end
+end
